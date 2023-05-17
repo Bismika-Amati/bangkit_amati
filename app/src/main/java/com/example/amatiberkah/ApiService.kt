bc6a29e70af2832fdfb06d7ef925b36e5cc791ba
cakuakz <rafiirvansyah88@gmail.com>
2023-05-17 21:37:13 +0700

    commit kedua

--- app/src/main/java/com/example/amatiberkah/ApiService.kt
+++ app/src/main/java/com/example/amatiberkah/ApiService.kt
@@ -1,5 +1,21 @@
 package com.example.amatiberkah
 
+import retrofit2.Call
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface ApiService {
+
+    @FormUrlEncoded
+    @POST
+    fun register (
+        @Field("name") name: String,
+        @Field("email") email: String,
+        @Field("phone") phone: String,
+        @Field("password") password: String,
+        @Field("role") role: String,
+    ) : Call<GeneralResponse>
+
 
 }
