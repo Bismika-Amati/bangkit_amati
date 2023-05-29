fc054c77fc98eff81f98a370a1c73f2f81f940bf
cakuakz <rafiirvansyah88@gmail.com>
2023-05-29 22:58:29 +0700

    commit 45% view explore

---
+++ app/src/main/java/com/example/amatiberkah/view/ExploreActivity.kt
@@ -0,0 +1,14 @@
+package com.example.amatiberkah.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.amatiberkah.R
+
+class ExploreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explore)
+
+
+    }
+}
